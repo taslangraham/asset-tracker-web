d3e58b44f03b582c086206efb127ced3541a48c2
taslangraham <gtaslan1@gmail.com>
2020-06-09 14:04:27 -0500

    fixed issue of loading on http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,11 +23,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Schema::defaultStringLength(191);
         if (config('app.env') === 'production') {
-            URL::forceScheme('https');
+            $url->forceSchema('https');
         }
     }
 }
